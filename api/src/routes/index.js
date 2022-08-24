c2115a3f180b636a85c04a8131e593daa117e5a3
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-24 17:50:04 -0400

    Revert "Crear las rutas principales en el Backend"
    This reverts commit d5e0eeaf226ebeec5da353a062b011514d6a5419.

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,9 +2,6 @@
 const { Router } = require('express');
 const router = Router();
 
-router.use("/catalog", require("./catalog"));
-router.use("/authors", require("./authors"));
-router.use("/categories", require("./categories"));
-router.get('/',(req,res) => res.send("Bienvenido al backend de este precioso proyecto!"));
+router.get('/',(req,res) => res.send("Bienvenido al backend de este precioso proyecto!"))
 
 module.exports = router;
