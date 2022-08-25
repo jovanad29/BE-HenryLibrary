8cc3834e9b06ef4d79f88b75a1188213e2f04337
hcamusso <hcamusso@gmail.com>
2022-08-25 06:46:23 -0300

    Crear el cascaron de las rutas en index

---
+++ api/src/routes/review.js
@@ -0,0 +1,10 @@
+//definir la ruta de la api hacia el controlador de review
+const { Router } = require('express');
+const router = Router();
+// requerir los metodos del controlador de review
+
+// definir los metodos de la ruta de la api hacia el controlador de review
+
+
+//exportar el router para poder usarlo en el index.js
+module.exports = router;
