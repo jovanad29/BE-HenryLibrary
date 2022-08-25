4bce23777cfc4787dc982db4742c68343026f803
hcamusso <hcamusso@gmail.com>
2022-08-25 07:35:05 -0300

    Crear el metodo getAll de la ruta review

--- api/src/routes/review.js
+++ api/src/routes/review.js
@@ -2,8 +2,19 @@
 const { Router } = require('express');
 const router = Router();
 // requerir los metodos del controlador de review
-
+const {getAll} = require('../controllers/review');
 // definir los metodos de la ruta de la api hacia el controlador de review
+router.get('/', async (req, res) => {   
+    try {
+        let dbReviews = await getAll();
+        dbReviews
+            ? res.status(200).json(dbReviews)
+            : res.status(501).json({ message: 'No se encontraron reviews' });
+    } catch (err) {
+        console.log(err);
+        res.status(404).json(err);
+    }
+} );
 
 
 //exportar el router para poder usarlo en el index.js
