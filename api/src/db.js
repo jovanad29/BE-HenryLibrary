71823062123cf03405d136388e645f7af8f3bb2a
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-26 09:20:28 -0400

    Delete Apibook

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,6 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const {
-  Apibook,
   Book, 
   Category, 
   Author,
