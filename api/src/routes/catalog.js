6481687a438feca2e5bada063b83217d20864e2b
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2022-08-27 09:26:09 -0300

    filtrar por autor y categoria  correccion

--- api/src/routes/catalog.js
+++ api/src/routes/catalog.js
@@ -1,7 +1,9 @@
 //definir la ruta de la api hacia el controlador de catalogo
 const { Router } = require('express');  
 const router = Router();
-const {getAll,getBook,getById,createBook,updateBook,logicalDeleteBook} = require('../controllers/catalog');
+const {getAll,getBook,getById,createBook,
+  updateBook, getBookByAuthor, getBookByCategory,
+   logicalDeleteBook} = require('../controllers/catalog');
 
 router.get('/:id', async (req,res) => {
     const book = await getById(req.params.id);
@@ -37,6 +39,48 @@ router.get('/', async (req, res) => {
     }
   });
 
+//filter by author
+router.get('/author/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id) {
+      let book = await getBookByAuthor(id);
+      book
+        ? res.status(200).json(book)
+        : res.status(501).json({ message: 'No se encontraron libros para ese author' });
+    } else {
+      let dbBooks = await getAll();
+      dbBooks
+        ? res.json(dbBooks)
+        : res.status(404).json({ message: 'No se encontraron libros' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(502).json(err);
+  }
+});
+
+//filter by category
+router.get('/category/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id) {
+      let book = await getBookByCategory(id);
+      book
+        ? res.status(200).json(book)
+        : res.status(501).json({ message: 'No se encontraron libros para ese género' });
+    } else {
+      let dbBooks = await getAll();
+      dbBooks
+        ? res.json(dbBooks)
+        : res.status(404).json({ message: 'No se encontraron libros' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(502).json(err);
+  }
+});
+
 
 router.post('/', createBook);
 router.put('/:id', updateBook);
@@ -49,7 +93,7 @@ router.put('/delete/:id', async (req, res) => {
       let book = await logicalDeleteBook(id);
       book
         ? res.status(200).json(book)
-        : res.status(501).json({ message: 'No se encontro el libro a eliminar' });
+        : res.status(404).json({ message: 'No se encontro el libro a eliminar' });
     } else {
       let dbBooks = await getAll();
       dbBooks
