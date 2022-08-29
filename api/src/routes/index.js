0ae7481e10054cc470679e562447be5a8017054f
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-28 21:19:58 -0400

    Move index route to top

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,8 @@
 const { Router } = require('express');
 const router = Router();
 
+
+router.get('/',(req,res) => res.send("Bienvenido al backend de este precioso proyecto!"));
 router.use("/catalog", require("./catalog"));
 router.use("/authors", require("./authors"));
 router.use("/categories", require("./categories"));
@@ -10,8 +12,5 @@ router.use("/review", require("./review"));
 router.use("/payment", require("./payment"));
 router.use("/publisher", require("./publisher"));
 router.use("/paymentMethod", require("./paymentMethod"));
-
-
-router.get('/',(req,res) => res.send("Bienvenido al backend de este precioso proyecto!"));
 
 module.exports = router;
