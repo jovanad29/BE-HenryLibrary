3bc9d418c317a8e4cea8c53ab08c23f225479eb1
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-28 23:59:01 -0400

    Fix grammar

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,11 +4,11 @@ const router = Router();
 
 
 router.get('/',(req,res) => res.send("Bienvenido al backend de este precioso proyecto!"));
-router.use("/catalog", require("./catalog"));
+router.use("/catalogue", require("./catalogue"));
 router.use("/authors", require("./authors"));
 router.use("/categories", require("./categories"));
 router.use("/user", require("./user"));
-router.use("/review", require("./review"));
+router.use("/reviews", require("./reviews"));
 router.use("/payment", require("./payment"));
 router.use("/publisher", require("./publisher"));
 router.use("/paymentMethod", require("./paymentMethod"));
