f9ee447541d362b8521ebea1c8d0a31b8a179686
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-09-01 19:26:25 -0400

    Add port

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 
 const server = require('./src/app.js');
+require('dotenv').config();
+const { PORT } = process.env;
 
-server.listen(3001, async () => {
+
+server.listen(PORT, async () => {
 	console.log('%s listening at 3001'); // eslint-disable-line no-console
 });
