3be678580ac44f0a254b42e5d857e79fd23e977a
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-09-01 21:27:25 -0400

    Add url front

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,8 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 require('./db.js');
 const server = express();
-
+require('dotenv').config();
+const { FRONT_SERVER} = process.env;
 
 server.name = 'API';
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
@@ -14,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `${ FRONT_SERVER || 'http://localhost:3000'}`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
