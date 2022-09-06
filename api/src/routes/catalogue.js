b53fa35041bc694e7bb120b67e02403abc71dced
YepesF <yepilef@gmail.com>
2022-09-06 01:31:23 -0500

    Crear ruta para filtros combinados

--- api/src/routes/catalogue.js
+++ api/src/routes/catalogue.js
@@ -1,46 +1,48 @@
 //definir la ruta de la api hacia el controlador de catalogo
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 const {
-    getAll,
-    getBook,
-    getById,
-    createBook,
-    updateBook,
-    logicalDeleteBook,
-    getBookQty
-} = require('../controllers/catalogue');
+  getAll,
+  getBook,
+  getById,
+  createBook,
+  updateBook,
+  logicalDeleteBook,
+  getBookQty,
+  getBooksCategoryAuthor, //<--
+} = require("../controllers/catalogue");
 
+router.get("/", async (req, res) => {
+  const { title } = req.query;
+  const { pagina = 0, items = 10 } = req.query;
+  try {
+    if (title) {
+      let book = await getBook(title, pagina, items);
+      book.length
+        ? res.status(200).json(book)
+        : res.json({ status: 404, message: "No se encontró el libro" });
+    } else {
+      let dbBooks = await getAll(pagina, items);
+      dbBooks.length
+        ? res.json(dbBooks)
+        : res.json({ status: 404, message: "No se encontraron libros" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+router.get("/filter/", getBooksCategoryAuthor); //<--
 
-router.get('/', async (req, res) => {
-    const { title } = req.query;
-    const { pagina = 0, items = 10 } = req.query;
-    try {
-        if (title) {
-            let book = await getBook(title, pagina, items);
-            book.length
-                ? res.status(200).json(book)
-                : res.json({status: 404, message: 'No se encontró el libro'});
-            } else {
-                let dbBooks = await getAll(pagina, items);
-                dbBooks.length
-                ? res.json(dbBooks)
-                : res.json({status: 404, message: 'No se encontraron libros'});
-            }
-        } catch (error) {
-            console.log(error);
-            res.status(500).json(error);
-        }
-    });
-router.get('/:id', getById);
+router.get("/:id", getById);
 
-router.get('/count/:true', getBookQty);
+router.get("/count/:true", getBookQty);
 
-router.post('/', createBook);
+router.post("/", createBook);
 
-router.put('/:id', updateBook);
+router.put("/:id", updateBook);
 
-router.delete('/:id', logicalDeleteBook);
+router.delete("/:id", logicalDeleteBook);
 
 //exportar el router para poder usarlo en el index.js
 module.exports = router;
