5fdede4b743a11835da541e48858617740561af9
Flori O <floroldani@hotmail.com>
2022-09-08 00:58:07 -0300

    Crear rutas review

--- api/src/controllers/reviews.js
+++ api/src/controllers/reviews.js
@@ -1,5 +1,5 @@
 
-const { Review } = require('../db');
+const { Review, User } = require('../db');
 
 
 //----------- GET -----------//
@@ -7,6 +7,11 @@ exports.getAll = async (req, res) => {
     try {
         const reviews = await Review.findAll({
             order:[['id']],
+            include: {
+                model: User, 
+                attributes: ['uid',"nameUser","email"],
+                through: { attributes: [] }
+            }
         });
         // const orderReviews = reviews.map(review => review.toJSON());
         if (reviews) return res.json(reviews)
@@ -19,8 +24,86 @@ exports.getAll = async (req, res) => {
 
 //----------- POST -----------//
 
+exports.createReview = async function (req, res) {
+
+    const { uid, rating, descrption } = req.body
+    //uid -> id del user
+
+    const newReview = await Review.create({
+        rating,
+        descrption
+    })
+
+    try{
+
+        if(uid){ //UN COMENTARIO ES DE UN USUARIO
+            const userFind = await User.findOne({
+                where:{
+                    uid
+                   } 
+                }) 
+
+            userFind.addReview(newReview) //Aca uno el usuario al review
+        }
+
+        return res.status(201).json(newReview);
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
+} 
 
 //----------- PUT -----------//
 
+exports.editReview = async (req, res) => {
+
+    const { uid, id, rating, descrption } = req.body
+
+    const userUid = await User.findOne({ where:{ uid }})
+
+    try {
+        if(userUid){
+
+            await Review.update({
+                rating,
+                descrption
+            }, {where: { id }  }
+            );
+
+
+        } else {
+            res.status(404).json({status: 404, message: 'No se encontre el user'});
+        }
+        return res.status(204).json({});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
 
 //----------- DELETE -----------//
+
+
+exports.deleteReview = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        if (id) {
+
+            await Review.destroy({
+                    where: { id: id }
+                }
+            );
+
+            res.json({msg: "Review deleted with exit"});
+
+        } else {
+            res.status(404).json({msg:"Not recived id"})
+        }
+    } catch (error) {
+    res.send({msg:"The data cannot be deleted"});
+ }
+}
+    
--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -2,11 +2,17 @@
 const { Router } = require('express');
 const router = Router();
 // requerir los metodos del controlador de review
-const { getAll } = require('../controllers/reviews');
+const { getAll, createReview, editReview, deleteReview } = require('../controllers/reviews');
 
 
 // definir los metodos de la ruta de la api hacia el controlador de review
 router.get('/', getAll);
+
+router.post("/", createReview)
+
+router.put("/", editReview)
+
+router.delete("/:id", deleteReview)
 
 //exportar el router para poder usarlo en el index.js
 module.exports = router;
