f392628159f95356dbd065f9036d6ce04f52ed0b
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-09-08 12:03:37 -0400

    Cambiar comillas dobles por simples

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,5 +1,5 @@
 //definir la ruta de la api hacia el controlador de user
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
 const {
   getAll,
@@ -9,17 +9,18 @@ const {
   updateAdminUser,
   logicaldeleteUser,
   bannedUser,
-} = require("../controllers/users");
+} = require('../controllers/users');
 
-router.get("/", getAll);
-router.get("/byname", getUserByName);
-router.get("/:uid", getById);
+router.get('/', getAll);
+router.get('/byname', getUserByName);
+router.get('/:uid', getById);
 
-router.get("/", getUserByName);
-router.post("/", createUser);
-router.put("/:uid", updateAdminUser);
+router.get('/', getUserByName);
+router.post('/', createUser);
+router.post()
+router.put('/:uid', updateAdminUser);
 
-router.delete("/:uid", logicaldeleteUser);
-router.delete("/banned/:uid", bannedUser);
+router.delete('/:uid', logicaldeleteUser);
+router.delete('/banned/:uid', bannedUser);
 
 module.exports = router;
