237e52fbdc5f413f63c7c5e922d3f12f2df341bc
Jovana Davalillo <jovana.davalillo@gmail.com>
2023-04-03 14:05:27 -0300

    Update .env variables

--- api/src/db.js
+++ api/src/db.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const path = require('path');
 
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 // const { DATABASE_URL } = process.env
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`, {
 // const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  port: DB_PORT,
+  port: PGPORT,
   define: {
     freezeTableName: true,  // Mantiene los nombres definidos en los modelos (no los cambia a plural)
     // timestamps: false    // Comentar si se quieren crear los campos createdAt y updatedAt de forma predeterminada en todas las tablas
